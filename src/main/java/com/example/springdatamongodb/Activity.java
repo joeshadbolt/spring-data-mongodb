d4a896f48f1bfe19e53337f5f6d90fa68d392fe3
Dan Cebotarenco <dcebotarenco@gmail.com>
2023-08-24 23:12:29 +0300

    refactor(relation): backward link

--- src/main/java/com/example/springdatamongodb/Activity.java
+++ src/main/java/com/example/springdatamongodb/Activity.java
@@ -6,6 +6,7 @@ import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 @Data
 @Document(collection = "activities")
@@ -14,6 +15,7 @@ public class Activity {
     private ObjectId id;
 
     @NonNull
+    @DocumentReference
     private Engagement engagement;
 
 }
